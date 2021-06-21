b216c8fcbb252740ad4ce4bbe7945cbfc8e48b6f
Thatdoperabi <Thatdoperabi@gmailcom>
2021-06-21 16:14:45 -0500

    Final changes for project

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/HomeController.java
@@ -18,6 +18,7 @@ public class HomeController {
         HashMap<String, String> actionChoices = new HashMap<>();
         actionChoices.put("search", "Search");
         actionChoices.put("list", "List");
+       
 
         model.addAttribute("actions", actionChoices);
 
